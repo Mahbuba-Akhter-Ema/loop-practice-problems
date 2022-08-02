855a7f37b846d1552aa910fe5a3f0c8225dead2a
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-08-03 01:45:29 +0600

    first commit

---
+++ revision.js
@@ -0,0 +1,34 @@
+/*Type Of Variables*/
+var bottle = 'water';
+// Numaric type variable
+var bottlePrice = 200;
+// string type variable
+var bottleColor = 'Blue';
+var sunglassPrice = 200;
+var pen = 'black';
+// Boolean type variable
+var isHappy = 'false';
+
+/* To Create an array */
+var names = ['Ema', 'Saima', 'Dina', 'Bushra', 'Farabe', 'Mahdiyah']
+
+/* To check Index: index start from 0 */
+var numbers = [5, 10, 15, 20, 25, 30];
+var check3 = numbers[3];
+/* To replace element */
+numbers[4] = 100;
+/* To know index of an element */
+var emaIndex = names.indexOf('Ema');
+
+// Push/ Pop
+
+// Conditionals
+if (names.length > 4) {
+    console.log("I'm a bed girl");
+} 
+else if (condition) {
+    
+}
+else {
+    
+}
---
+++ while.js
@@ -0,0 +1,13 @@
+// Rules For Loop Variable // 
+/*************************** 
+* Loop variable
+* condition inside while
+* Loop body
+* Change the loop variable
+***************************/
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast Den Please!!');
+    roastGiven++;
+}
